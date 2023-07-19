66230d89f6a6c8b0873cdf020896d084b0228282
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-07-19 23:59:48 +0630

    feat: add header footer

--- app/page.js
+++ app/page.js
@@ -7,11 +7,15 @@ import {
 
 export default function Home() {
   return (
-    <main className="container mx-auto min-h-screen">
-      <nav className="mt-8 flex items-center justify-between lg:mt-12">
-        <BsFillBootstrapFill className="cursor-pointer text-3xl text-white" />
-        <BsFillMoonStarsFill className="cursor-pointer text-2xl text-white" />
-      </nav>
-    </main>
+    <>
+      <header>
+        <nav className="mt-8 flex items-center justify-between lg:mt-12">
+          <BsFillBootstrapFill className="cursor-pointer text-3xl text-white" />
+          <BsFillMoonStarsFill className="cursor-pointer text-2xl text-white" />
+        </nav>
+      </header>
+      <main className="container mx-auto min-h-screen"></main>
+      <footer></footer>
+    </>
   );
 }
