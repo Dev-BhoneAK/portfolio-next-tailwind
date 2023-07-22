051bda05c0eabe375ef200d0c0f0e3d9c9c71fce
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-07-22 23:54:18 +0630

    remove: change component directory

--- app/page.js
+++ app/page.js
@@ -1,19 +1,10 @@
 import Image from "next/image";
-import {
-  BsFillBootstrapFill,
-  BsTypeBold,
-  BsFillMoonStarsFill,
-} from "react-icons/bs";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <>
-      <header>
-        <nav className="mt-8 flex items-center justify-between lg:mt-12">
-          <BsFillBootstrapFill className="cursor-pointer text-3xl text-white" />
-          <BsFillMoonStarsFill className="cursor-pointer text-2xl text-white" />
-        </nav>
-      </header>
+      <Header />
       <main className="container mx-auto min-h-screen"></main>
       <footer></footer>
     </>
