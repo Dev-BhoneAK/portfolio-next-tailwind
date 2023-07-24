d21bceb4f194520c2d0dc1d88dc2d7517bbdcd54
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-07-24 23:19:57 +0630

    fix: change light/dark switch

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,15 +1,39 @@
+"use client";
 import {
   BsFillBootstrapFill,
   BsTypeBold,
   BsFillMoonStarsFill,
+  BsSunFill,
 } from "react-icons/bs";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
   return (
     <header>
       <nav className="mt-8 flex items-center justify-between lg:mt-12">
-        <BsFillBootstrapFill className="cursor-pointer text-3xl text-white" />
-        <BsFillMoonStarsFill className="cursor-pointer text-2xl text-white" />
+        <BsFillBootstrapFill className="cursor-pointer text-3xl" />
+        {theme === "dark" ? (
+          <BsFillMoonStarsFill
+            className="cursor-pointer text-2xl"
+            onClick={() => setTheme("light")}
+          />
+        ) : (
+          <BsSunFill
+            className="cursor-pointer text-2xl text-black"
+            onClick={() => setTheme("dark")}
+          />
+        )}
       </nav>
     </header>
   );
