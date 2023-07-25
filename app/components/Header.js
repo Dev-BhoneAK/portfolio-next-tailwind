2701e9cba18acf21b16122f2d2e146c622893640
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-07-25 07:35:18 +0630

    feat: add animation on dark mode switcher

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,6 +5,7 @@ import {
   BsFillMoonStarsFill,
   BsSunFill,
 } from "react-icons/bs";
+import { motion, AnimatePresence } from "framer-motion";
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
 
@@ -23,21 +24,29 @@ export default function Header() {
     <header>
       <nav className="mt-8 flex items-center justify-between lg:mt-12">
         <BsFillBootstrapFill className="cursor-pointer text-3xl" />
-        {theme === "dark" ? (
-          <div className="flex items-center justify-center bg-white rounded-full p-2">
-            <BsFillMoonStarsFill
-              className="cursor-pointer  text-lg text-blue-950"
-              onClick={() => setTheme("light")}
-            />
-          </div>
-        ) : (
-          <div className="flex items-center justify-center bg-blue-950 rounded-full p-2">
-            <BsSunFill
-              className="cursor-pointer text-lg text-sky-400"
-              onClick={() => setTheme("dark")}
-            />
-          </div>
-        )}
+        <div className="flex items-center justify-center bg-blue-950  rounded-full p-2 dark:bg-white">
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={theme}
+              initial={{ y: -10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: 15, opacity: 0 }}
+              transition={{ duration: 0.4 }}
+            >
+              {theme === "dark" ? (
+                <BsFillMoonStarsFill
+                  className="cursor-pointer  text-lg text-blue-950"
+                  onClick={() => setTheme("light")}
+                />
+              ) : (
+                <BsSunFill
+                  className="cursor-pointer text-lg text-sky-400"
+                  onClick={() => setTheme("dark")}
+                />
+              )}
+            </motion.div>
+          </AnimatePresence>
+        </div>
       </nav>
     </header>
   );
