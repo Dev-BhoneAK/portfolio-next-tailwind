25a1cecce4783bada2b7ecf6ca88abcedc6afc8d
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-08-02 23:52:37 +0630

    feat: add main section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 "use client";
-import KitchenDisplay from "../public/career/kitchen-display/kitchen-display.png";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import TransitionBackground from "./components/TransitionBackground";
-import { useTheme } from "next-themes";
-import { useState, useEffect } from "react";
 
 export default function Home() {
   const [mounted, setMounted] = useState(false);
@@ -17,7 +17,7 @@ export default function Home() {
 
   if (!mounted) {
     return null;
-  }git a
+  }
   return (
     <>
       <Header theme={theme} setTheme={setTheme} />
