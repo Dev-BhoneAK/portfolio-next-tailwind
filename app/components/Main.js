2e4d828b95e2a6a059d7e9a77fb366bb541eee95
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-08-04 23:50:43 +0630

    feat: add main components

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 import Image from "next/image";
-import ShwedagonProject from "./career/ShwedagonProject";
-import KitchenDisplayProject from "./career/KitchenDisplayProject";
-import TayProject from "./career/TayProject";
-import FootballMasterProject from "./career/FootballMasterProject";
-import EcommerceAdmin from "../../public/portfolio/ecommerce-admin.png";
+import ShwedagonProject from "./main/career/ShwedagonProject";
+import KitchenDisplayProject from "./main/career/KitchenDisplayProject";
+import TayProject from "./main/career/TayProject";
+import FootballMasterProject from "./main/career/FootballMasterProject";
+// import EcommerceAdmin from "../../public/portfolio/ecommerce-admin.png";
 import EcommerceConsumer from "../../public/portfolio/ecommerce.gif";
+import EcommerceConsumer2 from "../../public/portfolio/ecommerce2.gif";
 import EmployeePoll from "../../public/portfolio/employee-poll.png";
 import CharityProject from "../../public/portfolio/charity.gif";
+import InfotainmentProject from "../../public/portfolio/infotainment.png";
+import TestImage from "../../public/portfolio/web3.png";
 import PortfolioCard from "./common/PortfolioCard";
 
 export default function Main() {
@@ -30,11 +33,22 @@ export default function Main() {
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 text-center">
           <PortfolioCard
             title="Ecommerce Admin Dashboard"
-            image={EcommerceAdmin}
+            image={EcommerceConsumer2}
           />
           <PortfolioCard title="Ecommerce Website" image={EcommerceConsumer} />
           <PortfolioCard title="Charity Website" image={CharityProject} />
           <PortfolioCard title="Employee Poll" image={EmployeePoll} />
+          <div className="flex items-center justify-center col-span-2 border">
+            <div className="relative w-1/3">
+              <Image
+                // fill
+                alt="Ecommerce Admin Dashboard"
+                src={InfotainmentProject}
+                className="rounded-3xl "
+              />
+            </div>
+            {/* <PortfolioCard title="Infotainment" image={TestImage} /> */}
+          </div>
         </div>
       </section>
       {/* Portfolio Section */}
