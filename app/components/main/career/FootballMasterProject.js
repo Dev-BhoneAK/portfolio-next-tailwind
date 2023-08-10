86318ab67e8421325feca9a94f7d908422989049
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-08-10 23:51:52 +0630

    fix: auto scroll 3d carosel

--- app/components/main/career/FootballMasterProject.js
+++ app/components/main/career/FootballMasterProject.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { useMediaQuery } from "@/app/hooks/useMediaQuery";
 import { motion, AnimatePresence } from "framer-motion";
+import DreamLeague1 from "../../../../public/career/football-master/fm0.png";
+import DreamLeague2 from "../../../../public/career/football-master/fm1.png";
+import Livescore from "../../../../public/career/football-master/fm2.png";
 
 export default function FootballMasterProject() {
   // const Data = [
@@ -13,34 +16,8 @@ export default function FootballMasterProject() {
 }
 
 const SmallScreen = () => {
-  const Data = [
-    "/career/football-master/fm0.png",
-    "/career/football-master/fm1.png",
-    "/career/football-master/fm2.png",
-  ];
-  const [CenterId, setCenterId] = useState(0);
-  const [LeftId, setLeftId] = useState(Data.length - 1);
-  const [RightId, setRightId] = useState(1);
+  const [isCenter, setCenter] = useState("center");
 
-  const nextBtn = () => {
-    if (LeftId === Data.length - 1) {
-      setLeftId(0);
-    } else {
-      setLeftId(LeftId + 1);
-    }
-    if (CenterId === Data.length - 1) {
-      setCenterId(0);
-    } else {
-      setCenterId(CenterId + 1);
-    }
-
-    if (RightId === Data.length - 1) {
-      setRightId(0);
-    } else {
-      setRightId(RightId + 1);
-    }
-  };
-  const isPhoneScreen = useMediaQuery("(max-width: 420px)");
   // useEffect(() => {
   //   const id = setInterval(
   //     () =>
@@ -60,38 +37,98 @@ const SmallScreen = () => {
   // }, []);
   const variants = {
     center: {
-      x: "0rem",
-      opacity: 1,
-      scale: 1.2,
+      x: 0,
+      scale: 1.3,
       zIndex: "5",
       filter: "brightness(100%)",
-      backgroundImage: "url(" + Data[CenterId] + ")",
-      boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.3)",
+      // boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.3)",
+      transition: {
+        type: "spring",
+        duration: 1,
+      },
+    },
+    centerleft: {
+      x: "-100%",
+      scale: 1.1,
+      zIndex: "3",
+      filter: "brightness(40%)",
+      // boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.3)",
+      transition: {
+        type: "spring",
+        duration: 1,
+      },
+    },
+    centerright: {
+      x: "100%",
+      scale: 1.1,
+      zIndex: "3",
+      filter: "brightness(40%)",
+      // boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.3)",
       transition: {
         type: "spring",
         duration: 1,
       },
     },
     left: {
-      x: "1rem",
-      opacity: 1,
+      x: "8%",
       filter: "brightness(40%)",
-      scale: 1,
-      backgroundImage: "url(" + Data[LeftId] + ")",
+      scale: 1.1,
       zIndex: "4",
-      boxShadow: "unset",
+      // boxShadow: "unset",
+      transition: {
+        type: "spring",
+        duration: 1,
+      },
+    },
+    leftcenter: {
+      x: "100%",
+      scale: 1.3,
+      zIndex: "5",
+      filter: "brightness(100%)",
+      // boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.3)",
+      transition: {
+        type: "spring",
+        duration: 1,
+      },
+    },
+    leftright: {
+      x: "200%",
+      filter: "brightness(40%)",
+      scale: 1.1,
+      // boxShadow: "unset",
+      zIndex: "3",
       transition: {
         type: "spring",
         duration: 1,
       },
     },
     right: {
-      backgroundImage: "url(" + Data[RightId] + ")",
-      x: "0rem",
-      opacity: 1,
+      x: "-8%",
       filter: "brightness(40%)",
-      scale: 1,
-      boxShadow: "unset",
+      scale: 1.1,
+      // boxShadow: "unset",
+      zIndex: "3",
+      transition: {
+        type: "spring",
+        duration: 1,
+      },
+    },
+    rightcenter: {
+      x: "-100%",
+      scale: 1.3,
+      zIndex: "5",
+      filter: "brightness(100%)",
+      // boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.3)",
+      transition: {
+        type: "spring",
+        duration: 1,
+      },
+    },
+    rightleft: {
+      x: "-200%",
+      filter: "brightness(40%)",
+      scale: 1.1,
+      // boxShadow: "unset",
       zIndex: "3",
       transition: {
         type: "spring",
@@ -104,7 +141,7 @@ const SmallScreen = () => {
       {" "}
       <h3 className="text-xl text-center mb-2">2022</h3>
       <div className="h-3 w-3 rounded-full bg-pink-600 self-center mb-1"></div>
-      <div className="h-8 border-l-4 border-dotted self-center"></div>
+      <div className="h-8 border-l-6 border-dotted self-center border-sky-500"></div>
       <motion.div
         className="bg-[#112240] p-4 rounded-md shadow-md"
         initial={{ y: 100, opacity: 0 }}
@@ -113,39 +150,76 @@ const SmallScreen = () => {
         transition={{ duration: 1 }}
       >
         <div
-          className="flex items-stretch justify-center my-8 sm:my-12 h-60"
-          onClick={() => nextBtn()}
+          className="flex items-center justify-center mt-8 mb-12 sm:mt-12 sm:my-16"
+          onClick={() =>
+            setCenter((prevState) =>
+              prevState == "center"
+                ? "left"
+                : prevState == "left"
+                ? "right"
+                : "center"
+            )
+          }
         >
-          <AnimatePresence initial={false}>
-            <motion.div
-              key={LeftId}
-              variants={variants}
-              initial="center"
-              animate="left"
-              // exit={"leftHidden"}
-              className="w-full carousel-item"
-            ></motion.div>
-            <motion.div
-              variants={variants}
-              key={CenterId}
-              initial="right"
-              animate="center"
-              className="w-full carousel-item"
-            ></motion.div>
-            <motion.div
-              key={RightId}
-              variants={variants}
-              initial="left"
-              animate="right"
-              // exit={"rightHidden"}
-              className="w-full carousel-item"
-            ></motion.div>
-          </AnimatePresence>
+          <motion.div
+            initial="left"
+            animate={
+              isCenter == "left"
+                ? "leftright"
+                : isCenter == "right"
+                ? "leftcenter"
+                : "left"
+            }
+            variants={variants}
+            className="w-full sm:w-1/2"
+          >
+            <Image
+              src={DreamLeague1}
+              alt="Kitchen Display System"
+              className="rounded-xl sm:rounded-2xl"
+            />
+          </motion.div>
+          <motion.div
+            initial="center"
+            animate={
+              isCenter == "left"
+                ? "centerleft"
+                : isCenter == "right"
+                ? "centerright"
+                : "center"
+            }
+            variants={variants}
+            className="w-full sm:w-1/2"
+          >
+            <Image
+              src={DreamLeague2}
+              alt="Kitchen Display System"
+              className="rounded-xl sm:rounded-2xl"
+            />
+          </motion.div>
+          <motion.div
+            initial="right"
+            animate={
+              isCenter == "left"
+                ? "rightcenter"
+                : isCenter == "right"
+                ? "rightleft"
+                : "right"
+            }
+            variants={variants}
+            className="w-full sm:w-1/2"
+          >
+            <Image
+              src={Livescore}
+              alt="Kitchen Display System"
+              className="rounded-xl sm:rounded-2xl"
+            />
+          </motion.div>
         </div>
-        <h4 className="text-lg text-left text-slate-200 font-semibold">
-          Senior Fullstack Developer - Myanmar Media Linkage
+        <h4 className="text-xl text-left text-slate-200 font-semibold mb-2">
+          Senior Fullstack Developer - MML
         </h4>
-        <p className="text-slate-400 text-base">
+        <p className="text-slate-400 text-lg">
           Deliver high-quality, robust production code for a diverse array of
           projects for clients including SEA Games Equestrian 2013 in Myanmar,
           Shwedagon Pagoda Website is one of the donation of CE Technology among
@@ -153,7 +227,7 @@ const SmallScreen = () => {
           Shwedagon Pagoda Website is one of the projects that I particpated in.
         </p>
       </motion.div>
-      <div className="h-8 border-l-4 border-dotted self-center"></div>
+      <div className="h-8 border-l-6 border-dotted self-center border-sky-500"></div>
     </div>
   );
 };
@@ -172,7 +246,7 @@ const LargeScreen = () => {
       zIndex: "5",
       filter: "brightness(100%)",
       //   backgroundImage: "url(" + Data[CenterId] + ")",
-      backgroundImage: "url(/career/football-master/fm1.png)",
+      backgroundImage: "url(/career/football-master/dl2.png)",
 
       boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.3)",
       transition: {
@@ -188,7 +262,7 @@ const LargeScreen = () => {
       //   backgroundImage:
       // "url('https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=662&q=80')",
       //   backgroundImage: `${Livescore}`,
-      backgroundImage: "url(/career/football-master/fm0.png)",
+      backgroundImage: "url(/career/football-master/dl1.png)",
       zIndex: "4",
       boxShadow: "unset",
       transition: {
@@ -198,7 +272,7 @@ const LargeScreen = () => {
     },
     right: {
       //   backgroundImage: "url(" + Data[RightId] + ")",
-      backgroundImage: "url(/career/football-master/fm2.png)",
+      backgroundImage: "url(/career/football-master/livescore.png)",
       x: "6rem",
       opacity: 1,
       filter: "brightness(40%)",
