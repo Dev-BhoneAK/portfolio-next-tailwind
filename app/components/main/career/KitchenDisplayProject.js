7661bb043e6d15d3f2332717c28a929c055c605d
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-08-10 23:52:03 +0630

    fix: change text size

--- app/components/main/career/KitchenDisplayProject.js
+++ app/components/main/career/KitchenDisplayProject.js
@@ -17,7 +17,7 @@ const SmallScreen = () => {
       {" "}
       <h3 className="text-xl text-center mb-2">2015</h3>
       <div className="h-3 w-3 rounded-full bg-pink-600 self-center mb-1"></div>
-      <div className="h-8 border-l-4 border-dotted self-center"></div>
+      <div className="h-8 border-l-6 border-dotted self-center border-sky-500"></div>
       <motion.div
         className="bg-[#112240] p-4 rounded-md shadow-md"
         initial={{ y: 100, opacity: 0 }}
@@ -28,12 +28,12 @@ const SmallScreen = () => {
         <Image
           src={KitchenDisplay}
           alt="Kitchen Display System"
-          className="-mx-4 mb-4"
+          className="mb-4"
         />
-        <h4 className="text-lg text-left text-slate-200 font-semibold">
+        <h4 className="text-xl text-left text-slate-200 font-semibold mb-2">
           Web Developer - Novitee
         </h4>
-        <p className="text-slate-400 text-base">
+        <p className="text-slate-400 text-lg">
           Deliver high-quality, robust production code for a diverse array of
           projects for clients including SEA Games Equestrian 2013 in Myanmar,
           Shwedagon Pagoda Website is one of the donation of CE Technology among
@@ -41,7 +41,7 @@ const SmallScreen = () => {
           Shwedagon Pagoda Website is one of the projects that I particpated in.
         </p>
       </motion.div>
-      <div className="h-8 border-l-4 border-dotted self-center"></div>
+      <div className="h-8 border-l-6 border-dotted self-center border-sky-500"></div>
     </div>
   );
 };
@@ -77,10 +77,10 @@ const LargeScreen = () => {
         >
           <div className="h-3 min-w-[0.75rem] rounded-full bg-pink-600 mt-8"></div>
           <div className="bg-[#112240] p-4 rounded-md shadow-md ">
-            <h4 className="text-lg text-left text-slate-200 font-semibold mb-6">
+            <h4 className="text-xl text-left text-slate-200 font-semibold mb-6">
               Web Developer - Novitee
             </h4>
-            <p className="text-slate-400 text-base">
+            <p className="text-slate-400 text-lg">
               Deliver high-quality, robust production code for a diverse array
               of projects for clients including SEA Games Equestrian 2013 in
               Myanmar, Shwedagon Pagoda Website is one of the donation of CE
