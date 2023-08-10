2a38880dec16f04eee48ae2554e0e30c95136852
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-08-10 23:53:00 +0630

    fix: change spacing

--- app/components/common/ToolsTag.js
+++ app/components/common/ToolsTag.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ToolsTag({ tagName }) {
   return (
-    <span className="bg-blue-950 text-sky-500 px-2 py-2 mr-2 rounded-full capitalize text-sm font-semibold">
+    <span className="bg-blue-950 text-sky-500 px-3 py-2 mr-2 rounded-full capitalize text-sm font-semibold">
       {tagName}
     </span>
   );
