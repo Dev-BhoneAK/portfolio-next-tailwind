4def75312817a3b420f8d0dfacdc58dded85a70b
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-08-11 22:45:28 +0630

    fix: layout

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,22 +2,19 @@ import ShwedagonProject from "./main/career/ShwedagonProject";
 import KitchenDisplayProject from "./main/career/KitchenDisplayProject";
 import TayProject from "./main/career/TayProject";
 import FootballMasterProject from "./main/career/FootballMasterProject";
-// import EcommerceAdmin from "../../public/portfolio/ecommerce-admin.png";
-import EcommerceConsumer from "../../public/portfolio/ecommerce.gif";
-import EcommerceConsumer2 from "../../public/portfolio/ecommerce2.gif";
-import EmployeePoll from "../../public/portfolio/employee-poll.png";
-import CharityProject from "../../public/portfolio/charity.gif";
-import InfotainmentProject from "../../public/portfolio/infotainment.png";
-import TestImage from "../../public/portfolio/web3.png";
+
+import portfolioData from "../utils/portfolioData";
 import PortfolioCard from "./common/PortfolioCard";
-import PortfolioCard2 from "./common/PortfolioCard2";
+import EcommerceClient from "../../public/portfolio/ecommerce-client.gif";
+
+import OldPortfolioCard from "./common/OldPortfolioCard";
 
 export default function Main() {
   return (
     <main>
       {/* Career Section */}
-      <section className="space-y-10 md:space-y-20 mb-24 md:mb-28">
-        <h2 className="text-3xl text-center mb-2">My Career Projects</h2>
+      <section className="space-y-10 md:space-y-28 mb-24 md:mb-36">
+        <h2 className="text-3xl text-center">My Career Projects</h2>
         <ShwedagonProject />
         <KitchenDisplayProject />
         <TayProject />
@@ -26,17 +23,34 @@ export default function Main() {
       {/* Career Section */}
 
       {/* Portfolio Section */}
-      <section className="space-y-10 md:space-y-20 mb-24 md:mb-28">
+      <section className="space-y-10 md:space-y-28 mb-24 md:mb-36">
         <h2 className="text-3xl text-center">My Personal Projects</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-center">
-          <PortfolioCard
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 text-center">
+          {portfolioData.map((portfolio) => (
+            <PortfolioCard
+              title={portfolio.title}
+              image={portfolio.image}
+              description={portfolio.description}
+              tagNames={portfolio.tagNames}
+            />
+          ))}
+          {/* <OldPortfolioCard
             title="Ecommerce Admin Dashboard"
-            image={EcommerceConsumer2}
-          />
-          <PortfolioCard title="Ecommerce Website" image={EcommerceConsumer} />
+            image={EcommerceClient}
+          /> */}
+          {/* <PortfolioCard title="Ecommerce Website" image={EcommerceConsumer} />
           <PortfolioCard title="Charity Website" image={CharityProject} />
           <PortfolioCard title="Employee Poll" image={EmployeePoll} />
-          <PortfolioCard2 />
+          <PortfolioCard2 /> */}
+          {/* <Image
+            alt="Ecommerce Admin Dashboard"
+            src={Web6}
+            className="rounded-3xl"
+          /> */}
+          {/* <img
+            src="/public/portfolio/web6.png"
+            alt="Ecommerce Admin Dashboard"
+          /> */}
         </div>
       </section>
       {/* Portfolio Section */}
