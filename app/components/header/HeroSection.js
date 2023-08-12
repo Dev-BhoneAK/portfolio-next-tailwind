45c6615a22486c53c3ec5e9d0f17af3bf0108191
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-08-13 02:19:31 +0630

    feat: add animation

--- app/components/header/HeroSection.js
+++ app/components/header/HeroSection.js
@@ -1,22 +1,40 @@
 import ActionButton from "../common/ActionButton";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function HeroSection() {
+  const heroVariants = {
+    hidden: {
+      y: -20,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        ease: "easeInOut",
+        duration: 2,
+        delay: 1,
+      },
+    },
+  };
   return (
     <section className="flex items-center justify-center my-auto">
       <div className="text-center space-y-6">
-        <h4 className="text-lg tracking-widest">HELLO</h4>
-        <div>
-          <h1 className="text-4xl mb-2">
-            I'm Bhone Aung Kyaw,{" "}
-            <span className="text-pink-600">Full-stack Developer.</span>
-          </h1>
-          <h1 className="text-3xl md:text-4xl opacity-50">
-            From Yangon, Myanmar
-          </h1>
-        </div>
-        <h4 className="text-base md:text-lg uppercase tracking-normal sm:tracking-wide md:tracking-wider">
-          I make great things using react and Node
-        </h4>
+        <motion.div initial="hidden" animate="visible" variants={heroVariants}>
+          <h4 className="text-lg tracking-widest">HELLO</h4>
+          <div>
+            <h1 className="text-4xl mb-2">
+              I'm Bhone Aung Kyaw,{" "}
+              <span className="text-pink-600">Full-stack Developer.</span>
+            </h1>
+            <h1 className="text-3xl md:text-4xl opacity-50">
+              From Yangon, Myanmar
+            </h1>
+          </div>
+          <h4 className="text-base md:text-lg uppercase tracking-normal sm:tracking-wide md:tracking-wider">
+            I make great things using react and Node
+          </h4>
+        </motion.div>
         <ActionButton />
       </div>
     </section>
