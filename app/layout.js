a1b17be3c5c1310e92c59e452beb1bf6f4f77dab
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-08-14 23:09:15 +0630

    feat: import favicon

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,15 @@ import "./globals.css";
 import { League_Spartan } from "next/font/google";
 
 import Providers from "@/app/components/Providers";
-
 const leagueSpartan = League_Spartan({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of BAK",
+  title: "Hello",
   description: "My portfolio implemented using Next.js and Tailwind",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -17,7 +20,6 @@ export default function RootLayout({ children }) {
         className={`container mx-auto bg-white dark:bg-[#0a1a2f] text-[#0a1a2f] dark:text-sky-400 ${leagueSpartan.className}`}
       >
         <Providers>{children}</Providers>
-        {/* {children} */}
       </body>
     </html>
   );
