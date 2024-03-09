babd8d2830117f311984eec386500889b6210d8b
Bhone Aung Kyaw <bhoneak@outlook.com>
2024-03-09 23:52:26 +0630

    fix: change double quote char

--- app/components/main/career/ShwedagonProject.js
+++ app/components/main/career/ShwedagonProject.js
@@ -35,19 +35,12 @@ const SmallScreen = () => {
           Web Designer - SeaNet Technology
         </h4>
         <p className="text-slate-400 text-lg">
-          During my tenure at my initial company, I served as a Junior Web
-          Designer & Developer, where I actively contributed to various projects
-          and initiatives. A primary focus of my role was crafting frontend
-          solutions, mainly through the creation of HTML and CSS codebases. One
-          of the standout projects during my time there was the "Shwedagon
-          Pagoda Project," where I played a significant role in its development.
-          I assisted in the design efforts, collaborating closely with the team
-          to refine the project's visual elements and user experience.
-          Additionally, I engaged as a junior developer in the 2013 SEA Games
-          Equestrian event, contributing to the development of the event's point
-          system using C#. This experience allowed me to hone my skills and gain
-          valuable insights into software development processes in a dynamic
-          environment.
+          During the time of my initial company, I was a Junior Web Designer &
+          Developer, focusing on frontend solutions with HTML and CSS. I
+          contributed significantly to the &quot;Shwedagon Pagoda Project&quot;,
+          aiding in design and refining visual elements. I also worked as a
+          junior developer for the 2013 SEA Games Equestrian event, contributing
+          to the development of the event&apos;s point system using C# language.
         </p>
       </motion.div>
       <div className="h-8 border-l-6 border-dotted self-center border-sky-700"></div>
@@ -72,12 +65,13 @@ const LargeScreen = () => {
               Web Designer - SeaNet Technology
             </h4>
             <p className="text-slate-400 text-lg">
-              Deliver high-quality, robust production code for a diverse array
-              of projects for clients including SEA Games Equestrian 2013 in
-              Myanmar, Shwedagon Pagoda Website is one of the donation of SeaNet
-              Technology among others. Actively particpated in drawing web
-              design and software. Shwedagon Pagoda Website is one of the
-              projects that I particpated in.
+              During the time of my initial company, I was a Junior Web Designer
+              & Developer, focusing on frontend solutions with HTML and CSS. I
+              contributed significantly to the &quot;Shwedagon Pagoda
+              Project&quot;, aiding in design and refining visual elements. I
+              also worked as a junior developer for the 2013 SEA Games
+              Equestrian event, contributing to the development of the
+              event&apos;s point system using C# language.
             </p>
           </div>
 
