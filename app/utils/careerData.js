27adc1e189ac8de10b5d38716bf8cc7cfd1060b0
Bhone Aung Kyaw <bhoneak@outlook.com>
2024-03-10 23:58:54 +0630

    feat: add career data

---
+++ app/utils/careerData.js
@@ -0,0 +1,10 @@
+import ShwedagonPagoda from "../../public/career/shwedagon/shwedagon.png";
+
+export const SeaNetTechData = {
+  year: "2013",
+  position: "Web Designer - SeaNet Technology",
+  imageSrc: ShwedagonPagoda,
+  imageAlt: "Shwedagon Pagoda Project",
+  responsibility:
+    "During the time of my initial company, I was a Junior Web Designer & Developer, focusing on frontend solutions with HTML and CSS.I contributed significantly to the 'Shwedagon Pagoda Project', aiding in design and refining visual elements. I also worked as a junior developer for the 2013 SEA Games Equestrian event, contributing to the development of the event's point system using C# language.",
+};
