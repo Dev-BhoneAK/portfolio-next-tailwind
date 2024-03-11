3a5c6cec7e1bcffb3d6af2c46c5d41add05585b9
Bhone Aung Kyaw <bhoneak@outlook.com>
2024-03-11 23:38:43 +0630

    feat: add more career data

--- app/utils/careerData.js
+++ app/utils/careerData.js
@@ -1,10 +1,78 @@
 import ShwedagonPagoda from "../../public/career/shwedagon/shwedagon.png";
+import KitchenDisplay from "../../public/career/kitchen-display/kitchen-display.png";
 
 export const SeaNetTechData = {
   year: "2013",
   position: "Web Designer - SeaNet Technology",
+  companyWebsite: "https://www.seanetmsc.com/",
+  projectLinks: [
+    {
+      title: "Shwedagon Pagoda",
+      address: "https://shwedagonpagoda.com/",
+    },
+    {
+      title: "Digital Library",
+      address: "http://mahadhammapala.org/",
+    },
+  ],
   imageSrc: ShwedagonPagoda,
   imageAlt: "Shwedagon Pagoda Project",
   responsibility:
-    "During the time of my initial company, I was a Junior Web Designer & Developer, focusing on frontend solutions with HTML and CSS.I contributed significantly to the 'Shwedagon Pagoda Project', aiding in design and refining visual elements. I also worked as a junior developer for the 2013 SEA Games Equestrian event, contributing to the development of the event's point system using C# language.",
+    "During my time at my initial company, I served as a Junior Web Designer & Developer, specializing in frontend solutions using HTML and CSS. I made significant contributions to the 'Shwedagon Pagoda Project' by assisting in design and refining visual elements. Additionally, I worked as a junior PHP developer for the 'Maha Dhamma Pala e-Library' system, mainly contributed to enhancing the searching and browsing functions of the website.",
+};
+
+export const NoviteeData = {
+  year: "2015",
+  position: "Web Developer - Novitee",
+  companyWebsite: "https://www.novitee.com/",
+  projectLinks: [
+    {
+      title: "Kitchen Display",
+      address: "https://www.novitee.com/solutions/kitchen-display-system",
+    },
+    {
+      title: "Purchasing & Inventory",
+      address: "https://www.novitee.com/solutions/purchasing",
+    },
+  ],
+  imageSrc: KitchenDisplay,
+  imageAlt: "Kitchen Display Project",
+  responsibility:
+    "At Novitee, I held the position of Web Developer, where my primary responsibility was developing backend solutions using Laravel 4. Additionally, I utilized Bootstrap and jQuery to develop responsive and interactive frontend interfaces. I delivered high-quality code for projects like Purchasing and Inventory System, Hotel Booking System, and Kitchen Display System, for which you can see a visual representation.",
+};
+
+export const MMLFullStackData = {
+  year: "2018",
+  position: "Fullstack Developer - Myanmar Media Linkage",
+  companyWebsite: "https://mml.com.mm/",
+  projectLinks: [
+    {
+      title: "LoTaYa",
+      address: "https://lotaya.mpt.com.mm/",
+    },
+    {
+      title: "Click",
+      address: "http://52.77.69.3/",
+    },
+  ],
+  imageSrc: KitchenDisplay,
+  imageAlt: "Kitchen Display Project",
+  responsibility:
+    "I began as a fullstack developer at Myanmar Media Linkage, focusing on backend services using Laravel and NodeJS. This involved creating internal services like payment gateways and video transcoding service for products such as Click. Additionally, I maintained and enhanced lotayamm.com, a key product of Myanmar's leading telecom provider, MPT.",
+};
+
+export const MMLTeamLeadData = {
+  year: "2020",
+  position: "Senior Team Lead - Myanmar Media Linkage",
+  companyWebsite: "https://mml.com.mm/",
+  projectLinks: [
+    {
+      title: "Ball Sayar",
+      address: "https://www.novitee.com/solutions/kitchen-display-system",
+    },
+  ],
+  imageSrc: KitchenDisplay,
+  imageAlt: "Kitchen Display Project",
+  responsibility:
+    "At Novitee, I held the position of Web Developer, where my primary responsibility was developing backend solutions using Laravel 4. Additionally, I utilized Bootstrap and jQuery to develop responsive and interactive frontend interfaces. I delivered high-quality code for projects like Purchasing and Inventory System, Hotel Booking System, and Kitchen Display System, for which you can see a visual representation.",
 };
