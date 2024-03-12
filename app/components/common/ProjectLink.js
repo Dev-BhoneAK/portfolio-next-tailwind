87cd38f48a7fd53823261215e6751e0b3ae3e014
Bhone Aung Kyaw <bhoneak@outlook.com>
2024-03-12 23:59:18 +0630

    feat: add Project Link

---
+++ app/components/common/ProjectLink.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ProjectLink({ projectLinks }) {
+  projectLinks.map((projectLink) => <></>);
+  // return <div>ProjectLink</div>;
+}
+
+export default ProjectLink;
